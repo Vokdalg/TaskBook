0e23e0f9404fd7e9fe7613614ed3e1a59a568284
oleg <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>
2021-10-30 19:51:30 +0300

    Refactoring

--- Main.java
+++ Main.java
@@ -45,7 +45,8 @@ public class Main {
                             }
                         }
                     }
-
+                default:
+                    System.out.println("Некорректный вариант ввода!");
             }
         }
     }
--- Menu.java
+++ Menu.java
@@ -2,13 +2,11 @@ import java.util.Scanner;
 
 public class Menu {
     protected static int printMenu(Scanner scanner) {
-        System.out.print("""
-                Выберите действие:
-                1. Добавить задачу
-                2. Вывести список задач
-                3. Удалить задачу
-                0. Выход
-                """);
+        System.out.print("Выберите действие:\n" +
+                         "1. Добавить задачу\n" +
+                         "2. Вывести список задач\n" +
+                         "3. Удалить задачу\n" +
+                         "0. Выход\n");
         return scanner.nextInt();
     }
 }
